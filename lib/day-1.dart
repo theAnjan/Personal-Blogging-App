30ae3a2a7e985868a41d9aae7409cfcc1b1f2487
theAnjan <anjanpaudel.git@gmail.com>
2022-06-02 06:21:00 +0545

    day 2

--- lib/day-1.dart
+++ lib/day-1.dart
@@ -1,1 +1,9 @@
-var a = 3;
+WEEK 1 Day 1
+On Living the Hard Way
+
+Life's easy when you live it the hard way and hard if you try to live it the easy way.
+
+
+Self discipline means living your life the hard way:resisting temptations and instant gratification , in order to receive bigger and better rewards in the future. It's certainly easier to avoid all kinds of discomfort and indulg yourself whenever you want, but in the end all that you get from that approach is fleeting pleasure now at the expense of your future, which otherwise could have been much better.
+Consider a weak-willed person who, when faced with a challenge, immediately opts out. How likely is this person going to manage a crisis that they must face? Even a relatively insignificant problem can become an insurmountable obstracle for a person who's been living a sheltered life and always avoided what's difficult or disagreeable.
+Now contrast that with a person who voluntarily makes his or her life harder. They seek out and welcome challenges as opportunity to grow. Each self-imposed affliction strengthens them so that fewer and fewer difficulties in life overwhelm them. Day by day, they immunize themselves against problems, precisely because they seek them out. When life deals them an unexpected blow, they're ready to handle it because- thanks to living their lives the hard way- they're always ready for hardship.
---
+++ lib/day-2.dart
@@ -0,0 +1,9 @@
+Day-2
+On Your Choices
+Your choices are made in a moment, but their consequences will transcend a lifetime.
+
+Sleep in and barely get to work on time or wake up at 5 am to work on your side business before going to work at your day job? Stop trying the moment you get rejected or swallow your pride and keep going despite hearing 'no' dozen of times?
+It takes only a moment to make the wrong choice and jeopardize your future. What feels like an insignificant today can have a great, lasting impact on your future.
+Each choice sets a precedent and when you make the same wrong choice several times in a row, it becomes your standard modus operandi.
+While one hamburger every now and then isn't likely to ruin-your overall efforts to lose weight, underestimating the impact of repeatedly  making wrong choice can profoundly affect you over the course of the rest of your life.
+Each time you're faced with a decision between exerting self-discipline and taking it easy, remind yourself that the choice you're making today doesn't affect the present moment alone. A momentary decision can (and oftern will) reverberate for many years or even decades into future.
