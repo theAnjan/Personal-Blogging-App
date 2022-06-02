7902f2b6a2469d650bbb1d47696421d0a6f47f1f
theAnjan <anjanpaudel.git@gmail.com>
2022-06-03 05:12:31 +0545

    day 9

---
+++ lib/day-9.dart
@@ -0,0 +1,6 @@
+Day 9
+On your Future Self
+
+Your self-discipline is largely affected by your ability to feel empathy toward your future self. If the vision of you ten, twenty or thirty years from now isn't particularly vivid, you'll have a hard time denying yourself pleasure today so that the stranger in the future can benefit.
+Ask yourself if today you're grateful for the choices you made several years ago, or if you wish that in the past you had been more concerned about your future. Are the choices you're making today only benefit present 'you' but don't contribute or worse jeopardize your well being in the future?
+The person you'll become in ten years will most likely not be the same person you are today, but it will still be you and it's in your hands whether ten years from now you'll look back and feel glad you extended self-empathy well into the future or, find that you decided to be selfish and steal from your future self for some fleeting pleasure TODAY.
