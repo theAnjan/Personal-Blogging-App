fee2b7ac1962887572ceb64c04b462c53a3b4647
theAnjan <anjanpaudel.git@gmail.com>
2022-06-02 06:22:16 +0545

    day 4

---
+++ lib/day-4.dart
@@ -0,0 +1,6 @@
+Day-4
+On Creating Systems
+I value self-discipline, but creating systems that make it impossible to misbehave is more reliable than self-control.
+
+Sitting infront of a delicious food and staring for hours without eating is not self-discipline. They think of resisting temptations as being like a knight defending his kingdom against invader.
+The best way to protect yourself against temptations is to avoid them and for that old preparation is more valuable than self-control. Prepare yourself for difficult situations by putting up roadblocks ahead of time, when your resolve isn't being tested. Your self-control system will do heavy lifting for you, leaving your reserves of self-discipline to be used for the unplanned situations, when they arise.
