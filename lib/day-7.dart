b5140403b1a91a3c1ac9ffc3e1e69ed3629bb6a5
theAnjan <anjanpaudel.git@gmail.com>
2022-06-03 05:10:24 +0545

    day 7

---
+++ lib/day-7.dart
@@ -0,0 +1,7 @@
+Day 7
+On Poverty and self discipline
+Economic stress robs us of cognitive bandwidth.Poverty imposes a mental tax.
+
+Financial stress and all kind of stress diminishes your ability to delay gratificationl. You get stuck in a vicious cycle-different circumstance impose constant mental stress which then leads you to make bad decision in all aspect and the bad decision creates more stress.
+Build an emergency fund.
+Reduce stress which strengthen your ability to delay gratification and you can make more optimal choices favouring your future.
