94f368bc967bd38c0f8de112c2cd244edb232af0
theAnjan <anjanpaudel.git@gmail.com>
2022-06-02 06:30:45 +0545

    day 5

---
+++ lib/day-5.dart
@@ -0,0 +1,7 @@
+Day-5
+On Enslavement to self.
+Be sure that you are not slave to self-slavish thoughts, slavish desires and conduct slavish habits. Conquer these; cease to be a slave to self and no man will have the power to enslave you.
+
+It's easy to delegate the responsibility for our lives and choice to parents, to other people.
+While external circumstances can affect you in the end, whether or not they control your life depends on you. When you achieve self-mastery, no one and nothing will have the power to enslave you.
+The next time you blame an external factor for your lack of self-discipline, think again. Was it the person forced you at a gun point or you choose to do What you do.
