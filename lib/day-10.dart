a52be620bdb07c2ebbaaf541cc2460c7000fdd65
theAnjan <anjanpaudel.git@gmail.com>
2022-06-03 05:24:08 +0545

    day 10 original restored

--- lib/day-10.dart
+++ lib/day-10.dart
@@ -1,4 +1,4 @@
 Day 10
-On Building your storydfdfdf
+On Building your story
 
 Whenever you get discouraged, or feel tired by how far you still have to go to accomplish your goals, remind yourself that everybody who has build self-discipline had to  go through the same process -- starting with little changes which then turned into habits, which then led to big life style change and identity shifts, and eventually, into successes.
