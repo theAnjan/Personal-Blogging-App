a232db79df9a71cbe9de5abe585fe4632c1e0359
theAnjan <anjanpaudel.git@gmail.com>
2022-06-02 06:21:41 +0545

    day 3

---
+++ lib/day-3.dart
@@ -0,0 +1,7 @@
+Day-3
+On Being a Human
+Willpower- capacity to restrain our impulses resist temptaton- do what's right and good in long run rather what we want to do right now.
+
+Humans have the capacity to act against their urges in exchange for a better future. Unfortunately, many people live by the principle of "if it feels good, do it and if it does'nt , don't do it."
+Caving into your temptations whenever you feel them emerge is like relinquishing your humanity in a way. As an intelligent human being, you have an ability- and I daresay an obligation - to make decisions that are based on rational thinking, not on your instincts alone.
+Strive to be a better human and embrace your humanity by exercising your "willpower muscle" instead of succumbing to your most primal(and least helpful for your long-term goals) part of the brain. Your most primal instincts may provide temporary comfort, but seldom are good for the long term, except when there is a direct threat to your survival. 
