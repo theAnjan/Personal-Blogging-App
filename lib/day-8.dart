54955c9f5ee446a83f9a59e44e67fd0bee78ca4e
theAnjan <anjanpaudel.git@gmail.com>
2022-06-03 05:11:57 +0545

    day 8

---
+++ lib/day-8.dart
@@ -0,0 +1,7 @@
+Week 2 Day 8
+On Unessential Necessities
+We work hard to obtain something because we are convinced that we would be miserable without it.
+
+The problem with mistaking luxuries for necessities is that it's impossible to develop powerful self-discipline if you need everything to function.
+
+Develop your mental resilience and
