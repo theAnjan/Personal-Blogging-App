66649c42a31e630b1755b6925d97dc313471ae0e
theAnjan <anjanpaudel.git@gmail.com>
2022-06-04 05:48:48 +0545

    day 13

---
+++ lib/day-13.dart
@@ -0,0 +1,10 @@
+Day 13
+On what you want now and what you want most
+Discipline is choosing between what you want now and what you want most.
+
+Small negligence sets a precedent that reflects your preference in long run.
+Each time you make a choice that favors instant gratification, your behavior signals that you have weak motivators.
+What you want now ---------------------What you want most
+If two ends are close to each other you'll have a problem delaying gratification.
+If the thing you want most is so much more rewarding than what you want now, you'll have an easier time resisting the temptations.
+To achieve your long-term goals, make sure that the satisfaction you get from what you want most is always much stronger than the satisfaction you can get from what you want now.
