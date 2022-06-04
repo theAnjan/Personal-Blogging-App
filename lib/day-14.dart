60b7ff821d89d299907ddd9e1f5f2518e9e83126
theAnjan <anjanpaudel.git@gmail.com>
2022-06-04 05:49:28 +0545

    day 14

---
+++ lib/day-14.dart
@@ -0,0 +1,8 @@
+Day 14
+On Long Term Focus
+In order to succeed, you must have a long term focus. Most of the challenges in our lives come from a short term focus.
+
+I would have accomplished my goals more quickly by taking a more sustainable approach that would take me a year or two to reach my goal than fooling myself. I could reach it in two or three months or weeks.
+Analyze your goals and how you approach them.
+Replace short-term-oriented behaviors with those that show that you're in it for the long haul.
+You need self-discipline to maintain a long-term focus in all of your endeavors.
