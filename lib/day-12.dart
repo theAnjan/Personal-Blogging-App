a3bc6f0c320f1f25e0c1bde5753c5ffdfc6fdad2
theAnjan <anjanpaudel.git@gmail.com>
2022-06-04 05:48:10 +0545

    day 12

---
+++ lib/day-12.dart
@@ -0,0 +1,10 @@
+Day 12
+Calmness of mind
+
+Calmness of mind is a result of long and patient effort of self-control.
+Distractions lurk at every corner in today's busy and fast moving world.
+Meditation daily will increase self-control, reduce stress and make you feel happier.
+
+The key to developing everyday calmness is focusing your attention on the present moment or on a single activity that you're performing.
+Calmness and meditation lead to ability to control your state of mind and prevent emotions from clouding your judgement.
+Meditation for at least one hour before taking any big decisions || choices.
