8ccc3862b4e13c0012b0b7a1eb04e84452046b6b
theAnjan <anjanpaudel.git@gmail.com>
2022-06-04 05:47:06 +0545

    day 11

---
+++ lib/day-11.dart
@@ -0,0 +1,8 @@
+Day 11
+On self discipline and talent
+
+Self discipline without talent can often achieve astounding results, where as talent without self-discipline inevitably dooms itself to failure.
+Talent-while undeniably makes life easier-is but one part of the equation.
+By continiously pushing comfort zone, you develop high self confidence and overcome other fears in life.
+
+Before you complain that you don't have a talent for something or weren't "born this way", remind yourself that self-discipline in many situations, can more than make up for a lack of inborn traits.
