5aa674391967236235e8bac7ad24d0550a78764e
theAnjan <anjanpaudel.git@gmail.com>
2022-06-06 05:32:11 +0545

    day 24

---
+++ lib/day-24.dart
@@ -0,0 +1,9 @@
+Day 24
+On Happiness Through Self-Discipline
+
+For a person who has never tested their self-discipline over a long period of time, it's hard to believe that giving up pleasure can lead to immense happiness.
+A human being, reduced to it's primal form, has little ability for self-control. It will assume that every step outside the comfort zone is a danger and so you'll never grow.
+
+Exerting self-control is working against the dominance of your primal brain. You forego primal urges in order to reach bigger and more important goals or to cultivate values that are important to you.
+
+This leads to experiencing a wide variety of experiences that life has to offer. Many of them are immensly more gratifying than merely satisfying the most basic human needs.
