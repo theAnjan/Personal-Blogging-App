ed8a3ac76b1b18c3a80533df4231f8512a4597e4
theAnjan <anjanpaudel.git@gmail.com>
2022-06-05 06:17:43 +0545

    day 16

---
+++ lib/day-16.dart
@@ -0,0 +1,7 @@
+Day 16
+On self-Reliance
+
+A man must stand erect, not be kept erect by others.
+
+Your motivators should always start with you and your own resolve to make changes.
+External support can be valuable but you aren't self-disciplined enough unless you can stick to your resolutions even without the help of other people.
