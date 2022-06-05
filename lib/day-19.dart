2a9a3383c81a2bb94e8adb875bdf0f3567691b32
theAnjan <anjanpaudel.git@gmail.com>
2022-06-05 06:20:37 +0545

    day 19

---
+++ lib/day-19.dart
@@ -0,0 +1,7 @@
+Day 19
+On fighting well
+The essential thing is not triumph but the struggle, to have fought well.
+
+Nobody will ever give you any grades for your level of self-discipline. There's no finish line and there's no podium for the winners. The only purpose of building self-discipline is to conquer yourself-your own urges, your own weakness and your own self-sabotaging behaviors.
+The most important moments are the moments of struggle. When you're striving to fight even when you can barely stand and the whole world is spinning around you. It's this very act that proves your mettle and showers you with life-encompassing benefits, not the act of winning in itself.
+Whenever you find yourself frustrated that you're still a long way from the finish line, remember that it's right now at this very moment, that you're collecting the biggest rewards. It's the struggle in itself that improves you and makes you a more successful person.
