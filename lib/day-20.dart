6f827f58cd6a0e7c4def7142c701bb0dc881cec9
theAnjan <anjanpaudel.git@gmail.com>
2022-06-05 06:21:32 +0545

    day 20

---
+++ lib/day-20.dart
@@ -0,0 +1,7 @@
+Day 20
+On Taking small steps
+
+We should discipline ourselves in small things and from there progress to things of greater value.
+Rome wasn't build in a day and you won't build self-discipline overnight. If you've never been particularly disciplined, start small with easy challenges and then build on top of them.
+
+Working for just one minute longer when you're ready to call it a day.
