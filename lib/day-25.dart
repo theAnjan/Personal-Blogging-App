5f82022bf7551e189392b121d9e42ffedb9e3b38
theAnjan <anjanpaudel.git@gmail.com>
2022-06-06 05:33:24 +0545

    day 25 delay and inaction follows

---
+++ lib/day-25.dart
@@ -0,0 +1,8 @@
+Day 25
+On starting Today
+Don't wait for tomorrow to do something you can do today.
+
+Don't overthink and don't obsess over the future; take care of establishing a tiny habit today and take it from there.
+
+#Uncomfortable feel hune , garo feel hune, Delay garna khojne , man ko soch hunxa and inaction follows
+Solution: Take action at the very first moment manlai khelna nadiney, even it is wrong, action is better than inaction in long term.
