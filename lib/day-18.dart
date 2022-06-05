dbaf237656c89be8a73b3fee4aa1e806b2883129
theAnjan <anjanpaudel.git@gmail.com>
2022-06-05 06:19:57 +0545

    day 18

---
+++ lib/day-18.dart
@@ -0,0 +1,7 @@
+Day 18
+On Higher Standards
+
+Hold yourself responsible for a higher standard than anybody else expects of you. Never excuse yourself. Never pity yourself.
+Be a hard master to yourself.
+The only standards that should concern you are your own standards. If you act in accordance with the standards of majority of people, you'll be unfit, lazy, hating your job, not having enough time for your family and in debt.
+If you allow yourself to have low standards how are you supposed to ever achieve excellence? Exhibiting self-control is one of the most powerful demonstrations of having high standards; letting fleeting emotions and urges control your life-as most people do- is a sure-fire path to mediocrity.
