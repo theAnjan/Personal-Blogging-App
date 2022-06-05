0dd63a810bdb7a93a32c4b9e3454fee2024fe296
theAnjan <anjanpaudel.git@gmail.com>
2022-06-05 06:18:29 +0545

    day 17

---
+++ lib/day-17.dart
@@ -0,0 +1,6 @@
+Day 17
+On Rising from the Ashes of Failure.
+
+A setback has often cleaned the way for greater prosperity. Many things have fallen only to rise to more exalted heights.
+
+No matter how self-disciplined you are, there's no escaping the fact that sometimes you'll stumble. It's possible that when your efforts results in failure, you'll loose the self-discipline to continue and revert back to your old undesirable habits. It's all par for the course, and the sooner you accept it, the easier it will be to handle the setbacks. A failure can often present new opportunities or lead to important realization. When discouraged, remember that all struggles present opportunities that given enough time you can convert into successes or lessons that will aid you in other areas of life.
