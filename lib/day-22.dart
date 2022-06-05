969a453086f8b8ad4f4e88ebaeedf34a504e136a
theAnjan <anjanpaudel.git@gmail.com>
2022-06-06 05:30:17 +0545

    day 22 freedom

---
+++ lib/day-22.dart
@@ -0,0 +1,6 @@
+Week 4 Day 22
+On self Discipline as Freedom
+
+Self-discipline is a form of freedom, freedom from laziness and lethargy, freedom from weakness and fear or doubt. It allows you to feel inner strength, your talent. He is master of, rather than a slave to, his own thoughts and emotions.
+If you think of self-discipline as in terms of deprivation and suffering, you'll never find enjoyment in personal growth, and most likely will soon give up on your endeavors.
+The freedom to be master of your thoughts and emotions is ultimately worth more than any temporary gratification.
