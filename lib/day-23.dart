e6e8a5c07936ff52286f9c1c58ccacf1c82b33da
theAnjan <anjanpaudel.git@gmail.com>
2022-06-06 05:31:29 +0545

    day 23 mental resilience

---
+++ lib/day-23.dart
@@ -0,0 +1,7 @@
+Day 23
+On Disciplined Education
+The way you get information can affect your self-discipline.
+
+When you keep going, you train yourself to stay with problems for as long as necessary to figure them out instead of giving up.
+
+Mental laziness
