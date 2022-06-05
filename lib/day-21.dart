8929bd85c0cfbd5a63c2ca5b23dca003cc0c15e8
theAnjan <anjanpaudel.git@gmail.com>
2022-06-06 05:29:38 +0545

    day 21 importance of habit

---
+++ lib/day-21.dart
@@ -0,0 +1,11 @@
+Day 21
+On the Importance of Habits.
+Success is actually a short race-a sprint fueled by discipline just long enough for habit to kick in and take over.
+
+Habits are like magical power. The moment they kickin, you no longer need more than perhaps a modicum of self-discipline every now and then to perform same action on regular basis. What originally was extremely difficult is now something you do automatically, with little thought or willpower.
+When something becomes a part of your routine, resistance drops to nearly zero. The challenging part is forming a new habit. It takes around 18-254 days with 66 as average. When working on your goals, remind yourself that it's first months that will be the hardest.
+Once proper habits kick in, things will get easier.
+Remember that in the long term, only habits-and their subsequent permanent changes in lifestyle and identity-can ensure lasting success.
+Consequently, its key to think in terms of life long routines.
+As long as it is not a habit,you will revert back to inactivity, old days, PANAUTIS.
+Analyse your goals and assess whether your plans employ self-discipline to help you build a habit or if you're using self control as a means of temporarily sustaining an impermanent change.Change an impermanent change.
