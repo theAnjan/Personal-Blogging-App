c72ee0e82d613ca132904f4a0bbec2a512108b67
theAnjan <anjanpaudel.git@gmail.com>
2022-06-07 15:35:11 +0545

    day 26 choice

---
+++ lib/day-26.dart
@@ -0,0 +1,7 @@
+Day 26
+On the long term consequences of your choices.
+Whenever you are presented with a choice, ask yourself which option you would prefer to have taken in ten years.
+
+Self-discipline is largely dependent on your ability to look into the future and imagine yourself not having taken the difficult choice today.If you paint the mental picture with enough detail, you won't be able to bear the thought of letting the situation remain the same or getting worse.
+A simple though of which option you would prefer to have taken in ten years can help you avoid succumbing to temptations.And let's not fool ourself, it won't work every time but it will make a difference.
+Failure to stick to your resolutions sets a precedent that can ruin your long-term progress.
