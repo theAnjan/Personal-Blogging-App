2c72699803449a94016883128aea2dda212ff497
theAnjan <anjanpaudel.git@gmail.com>
2022-06-07 16:30:29 +0545

    day 29 week 5

---
+++ lib/day-29.dart
@@ -0,0 +1,8 @@
+Week 5 Day 29
+On Avoiding Effort Baishakh-29
+
+How often do you avoid effort when it would have been beneficial to face it?
+
+Thrive in a challenging environment.
+
+Make the best out of the worst situation.
