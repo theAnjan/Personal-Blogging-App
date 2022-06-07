7ebe34f87a4b64c4eac6a9960c0c43fca672d16e
theAnjan <anjanpaudel.git@gmail.com>
2022-06-07 16:29:29 +0545

    day 28

---
+++ lib/day-28.dart
@@ -0,0 +1,6 @@
+Day 28
+On Living in offensive mode.
+
+You can risk launching a side business because even if it fails, you'll still have a secure job and perhaps even some savings.
+
+Life is an ongoing process of choosing between safety(out of fear and need for defense) and risk(for the sake of progress and growth). Make the growth choice a dozen times a day.
