599549f73ef5d99ceeac79eaf5f8d6cda3deb5f2
theAnjan <anjanpaudel.git@gmail.com>
2022-06-07 15:36:06 +0545

    day 27 wrong

---
+++ lib/day-27.dart
@@ -0,0 +1,6 @@
+Day 27
+On Following the wrong path
+
+If something scares you in a excited way, that's a good sign. But if it is making you miserable and draining your energy please stop. Life is telling you that it is not the path for you.
+
+Self-discipline is powerful as long as you apply it to the goals you care about- the ones that, even when they are difficult to accomplish, energize you.
