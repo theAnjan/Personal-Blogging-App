7b6c8823cb6163e59bd93bee7fb9b043054df761
theAnjan <anjanpaudel.git@gmail.com>
2022-06-09 05:32:55 +0545

    day -33

---
+++ lib/upto40/day-33.dart
@@ -0,0 +1,8 @@
+Day 33
+On Things You Can't Rush
+
+Patience and self-discipline. If you want to reject instant gratification for the sake of accomplishing your long term goal, it might take months or years before you receive your big compensation.
+Without patience, you'll get nowhere. While sometime you can force harder-Oftentimes things take time and there's no way to rush them, no matter how many resource you apply. If you fail to identify which things cannot be rushed, you might mis-apply the power of sel discipline and instead of achieving them more quickly,fail to achieve them at all.
+Rewiring your brain takes time.
+You can't force yourself to change overnight, it doesn't matter how much time you spend meditating or reading books about emotional control, a process that is so profound takes time.
+If it has taken you years or decades to develop a negative trait, don't expect that you can reverse it within weeks or months.
