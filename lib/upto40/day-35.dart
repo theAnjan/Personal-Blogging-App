3dbabe1142cf8466330901a940486104d5ff2886
theAnjan <anjanpaudel.git@gmail.com>
2022-06-09 05:34:09 +0545

    day 34

---
+++ lib/upto40/day-35.dart
@@ -0,0 +1,5 @@
+Day 35
+On the value of Difficulty
+What we obtain too cheap, we esteem too lightly.
+
+Over the long term, make sure that you always have at least one big, ambitious and demanding goal in life, as that's where the power of self-discipline shines, where most personal growth happens and which delivers the greatest feeling of having accomplished something worth doing 
