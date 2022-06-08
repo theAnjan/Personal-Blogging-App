7bad96f6bb0502d1b5610a7307ba2149d3e68514
theAnjan <anjanpaudel.git@gmail.com>
2022-06-09 05:30:34 +0545

    day 31

---
+++ lib/upto40/day-31.dart
@@ -0,0 +1,10 @@
+Day 31
+On Being Normal
+deadly and insupportable boredom, ahell of sterility and hopelessness
+
+Most people avoid all kind of discomfort and effort. Spending money they don't have on things they don't need.
+For self-discipline, you will be considered weird.This means you need to prepare yourself for potential ridicule, being frowned upon and not being understood.
+
+It might be hard at first to face so much adversity when all you want to do is to improve yourself. Normal is for the medicore, weird is for the high-achievers and trailblazers.
+
+Every time you feel out of sync with the rest of the world, remember that there are other people like you. Whenever you're stumbling, remind yourself that even when you're failing, you're still forging your own path, something that the vast majority of people will never do. You can enjoy the fruits of your success in a way that they will never experience, and that's why it's worth it to be exceptional. Be exceptional.
