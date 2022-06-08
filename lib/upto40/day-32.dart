769c4b82a4518fbd2f432da53989f0e77331c9f1
theAnjan <anjanpaudel.git@gmail.com>
2022-06-09 05:32:22 +0545

    day 32

---
+++ lib/upto40/day-32.dart
@@ -0,0 +1,4 @@
+Day 32
+On cultivating self-discipline like a plant
+
+The moment you decide it's time to stop coasting through life and prioritize long-term rewards over instant gratification, this moment, is just the very beginning for a long journey.
