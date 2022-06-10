84107661a4297b1ff75a27e4dae3d45ae4049506
theAnjan <anjanpaudel.git@gmail.com>
2022-06-10 15:54:53 +0545

    week6day36

---
+++ lib/upto40/day-36.dart
@@ -0,0 +1,6 @@
+Week 6 Day 36
+On pushing your limits step by step
+
+There's no law stating that if you want to build self-discipline, you need to immediately wake up at four in the morning, eat nothing but vegetables and fruits, work productively for 12 hours, seven days a week and abstain from every pleasure, trying to mimic the life of a mediveal ascetic.
+
+Self-discipline isn't built in one day, you're building it step by step-Starting from an easy and then building on top of it.
