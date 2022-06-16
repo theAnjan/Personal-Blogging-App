0453e7d9a4de7a778bc697345cfe4452c8ca2b61
theAnjan <anjanpaudel.git@gmail.com>
2022-06-17 02:58:22 +0545

    day 38

---
+++ lib/upto40/day-38.dart
@@ -0,0 +1,6 @@
+Day 38
+On moderation as a Good Thing
+When you set firm resolutions to improve yourself, you might be tempted to push your limits well outside of what you're capable of doing. Ambitiousness is a virtue, but there's danger involved in going from one extreme to  another.
+
+Find the middle path, stick to it for at least several weeks, and then based on the results, you get decide whether you can further stretch your limits or require more time before advancing.
+Sometimes a more extreme approach is needed for a short period of time and sometimes it's beneficial to set your goals lower.
