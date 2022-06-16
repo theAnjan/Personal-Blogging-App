2170246f721406139bbe6241adc6c4d089dd25f5
theAnjan <anjanpaudel.git@gmail.com>
2022-06-17 02:57:31 +0545

    day 37

---
+++ lib/upto40/day-37.dart
@@ -0,0 +1,8 @@
+Day 37
+On initial Resistance
+It is easier to resist at the beginning than at the end.
+
+If you let yourself ponder whether I want to start working or not, my resistance will grow and soon, it will be so difficult to overcome that you fail to accomplish anything productive for the rest of the day.
+Once you start performing an unpleasant task and gain some momentum, it's easier to stick to the task than to give up and return to procastination.
+Whenever you find, catch yourself putting off something unpleasant, act right away and deal with it now. The sooner you act, the sooner you'll be done with it.
+
