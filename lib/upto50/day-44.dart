adbfc7f65e7599c0db595b3eb3eb0202b1c9d958
theAnjan <anjanpaudel.git@gmail.com>
2022-06-17 03:02:50 +0545

    day 44

---
+++ lib/upto50/day-44.dart
@@ -0,0 +1,9 @@
+Day 44
+On optimism
+Optimism is the faith that leads to achievement: nothing can be done without hope
+
+A positive attitude is essential if you want to build self-discipline. What's the point of denying yourself instant gratification if you don't believe that you'll get a greater compensation for it in the future?
+Become optimistic
+1.Express gratitude for what you already have.
+2.Reframe negative events into opportunity to change your life and lessons to give it a positive meaning.
+3.Surround yourself with positive input.If you hang out with pessimistic grumblers: you'll have a hard time exhibiting optimism.
