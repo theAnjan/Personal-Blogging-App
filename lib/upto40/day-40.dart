d02bdf729ba3168e732b10782420b930d10835a2
theAnjan <anjanpaudel.git@gmail.com>
2022-06-17 02:59:34 +0545

    day 40

---
+++ lib/upto40/day-40.dart
@@ -0,0 +1,4 @@
+Day 40
+On Talking vs Doing
+It is better to practice a little than talk a lot.
+Announcing to all of your friends, family members and colleagues that you're going to change and going deep into details how you're going to do it is useless at best and sabotaging at worst.
