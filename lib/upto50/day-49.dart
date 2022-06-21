7483dc719b189ed8edb8a9e54ebb82c4741d4780
theAnjan <anjanpaudel.git@gmail.com>
2022-06-21 13:34:34 +0545

    day 49

---
+++ lib/upto50/day-49.dart
@@ -0,0 +1,8 @@
+Day 49
+On obeying Lust
+Bad men obey their lust as Servants obey their master.
+A forbidden fruit is the sweetest. If it weren't so pleasant to submit to your urges, no body would ever struggle with self-discipline. Temptation, not withstanding how much pleasure it can bring, it's important to see Temptations for what it is- your enemy on the path toward freedom. Obeying your lust enslaves you, while rejecting them increases your freedom. The reward you'll get for not succumbing to your temptations will more than makeup for the price you pay today for missing out on the instant gratification.
+Self-disciplined people may appear to some as if they were the ones being enslaved. After all, they're the ones whose lives are so limited: they don't get to eat junk food, they follow a strict routine, deliberately expose themselves to discomfort, and reject what society considers the spice of life-gluttony, laziness and engagging in other vices.
+What the critics fail to see is that, through the rejection of those temptations, the self-disciplined become the master of their lives. They serve the goal chosen by themselves instead of fleeting, spontaneous temptations. They choose to forego temporary satisfactions for deeper, more lasting ones later on.
+In the mean time, in the long haul, the people who fail to control their urges-or rather, don't even try to control them-fail to control their lives, manipulated by the temptations like a marionette.
+It was necessary.
