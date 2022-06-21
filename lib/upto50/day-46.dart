b20e12c1b4c1cc5862babe5142bc0804991f9c4e
theAnjan <anjanpaudel.git@gmail.com>
2022-06-21 13:31:51 +0545

    day 46

---
+++ lib/upto50/day-46.dart
@@ -0,0 +1,7 @@
+Day 46
+On Looking Fear in the Face
+
+Self-discipline is a lot like dealing with fear. When you face tempttions and look straight in the eye with focus, you can handle...
+The more times you successfully overcome the temptations, the easier it will be to handle them again.
+Regular practice will make you less susceptible to temptations.
+Just remember that each situation that tests your resolve is another experience from which you can draw inspiration to overcome future challenging circumstances when they occur.
