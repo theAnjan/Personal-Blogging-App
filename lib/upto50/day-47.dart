79703592077397f1d64d60305c37a2450b731316
theAnjan <anjanpaudel.git@gmail.com>
2022-06-21 13:32:34 +0545

    day 47

---
+++ lib/upto50/day-47.dart
@@ -0,0 +1,8 @@
+Day 47
+On the Folly of Loafing Around
+The loafer believes he is enjoying life,but sooner or later he must face disillusion.
+Since self-discipline shines in long term, and oftren doesn't seem to provide any benefits in the short term, you may be tempted to believe that people who are loafing around have it better.
+
+It might seem they have it better, but they will get to feel negative consequences later.
+You will look back at your past sacrifice and smile, happy that you've never succumbed to the temptation to take it easy and loaf around.
+
