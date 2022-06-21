5f0e454b1b3582c24297b4d8934a2d0942355a69
theAnjan <anjanpaudel.git@gmail.com>
2022-06-21 13:35:11 +0545

    day 50

---
+++ lib/upto50/day-50.dart
@@ -0,0 +1,6 @@
+Day 50
+On Not Resting on Your Laurels
+To be human is to grow, challenge yourself.
+dwelling on Success or failure for two long can lead to resting on your laurels and jeopardizing your future growth.
+
+Success is not a permission to become lazy.
