6a42eb4d9c5aa50e3a935242a5ba6c52aba43424
ANJANPAUDEL <anjan.paudel@azrachit.com>
2022-07-09 10:53:58 +0545

    day 53

---
+++ lib/upto60/day-53.dart
@@ -0,0 +1,9 @@
+Day 53
+On Having a Burning "yes" Inside
+You have to decide what your highest priorities are.
+
+Without the bigger "Yes" burning inside of you, there's no way you'll continue making the uncomfortable choices over the long term for the sake of your goal.
+So that even after big defeat, you will never "lay down your arms". If you have such a strong "yes" inside you, you'll also refuse to surrender the fight to achieve your dreams.
+It helps to pinpoint your priorities and disregard distractions.
+
+Granted, there should be some balance in everything- sometime self help book sucks if followed blindly- so some cheat day or lowering expectation for some time or holidays are good to keep going and avoiding abstract burnout to exhaustion.
