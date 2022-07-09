4f8043a364d17c4f030c51048759256fbceda434
ANJANPAUDEL <anjan.paudel@azrachit.com>
2022-07-09 10:54:36 +0545

    day 54

---
+++ lib/upto60/day-54.dart
@@ -0,0 +1,10 @@
+Day 54
+On underestimating the Long Term Approach.
+We always overestimate the change that will occur in the next two years and underestimate the change that will occur in the next ten. Lulled into inaction.
+
+A staggering number of people giveup on their goals just weeks or months into them, discouraged that they still haven't reached their goals or that their results are lackluster. Sorry to have to break it, but the world doesn't work that way. Nobody can build a successful business in six months.
+Ten years of dedicated practice can turn anyone into a world-class expert. When you gain momentum, you'll get exponential results. The trick is to stick to your goals long enough for the velocity to accure.
+
+Remember overestimating what you can achieve in a short period of time can happen often.
+Whenever setting a new goal, take the long-term approach. Unlike the person who assumes that their world can change overnight, be in it for the long haul, and the short-term fluctuations won't make you give up.
+#
