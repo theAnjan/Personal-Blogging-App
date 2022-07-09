29f12c2917e8db7470956bf5d77cecc8cc7db5b6
ANJANPAUDEL <anjan.paudel@azrachit.com>
2022-07-09 10:55:38 +0545

    day 55

---
+++ lib/upto60/day-55.dart
@@ -0,0 +1,6 @@
+Day-55
+On Bearing Misfortune
+Hardship are a part of life and nobody enjoys pain, but they are valuable because they present an opportunity for personal growth.
+You discover self-discipline when you struggle, not when everything goes fine and well.
+
+Misfortune aren't fun to deal with, but bearing them nobly strengthens your ability to handle even worse circumstances in the future. In a sense, this is training for life. You may not enjoy it but you know that eventually it will pay off and more than recoup for any suffering you're going through at the moment.
