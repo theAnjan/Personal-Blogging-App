ae03e0679193b411064d2989d60732bd8b856bed
ANJANPAUDEL <anjan.paudel@azrachit.com>
2022-07-09 10:53:10 +0545

    day 52

---
+++ lib/upto60/day-52.dart
@@ -0,0 +1,9 @@
+Day 52
+On Thinking For yourself
+
+Self-discipline is hard to attain, and because it's not common, sooner or later somebody will deem your behaviour abnormal. If your behaviour differs from other then, you're weird. Do what is best for you.
+It's difficult to trust your own judgement when everybody around you is doing something different, but if everything you do in accordance with society's norms, then what you're going to get is the same results as everybody else does who follow those norms.
+* Few years of Grinding, life time of freedom.
+
+Trust your own judgement and think for yourself. It's better to suffer from your own choices than waste your life away because you were mindlessly following the herd.
+Remove refer copy
