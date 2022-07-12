5b1f4ec208eedca7fb4b4b617badb67c697c3463
ANJANPAUDEL <anjan.paudel@azrachit.com>
2022-07-12 21:35:36 +0545

    day 57

---
+++ lib/upto60/day-57.dart
@@ -0,0 +1,7 @@
+Week 9 Day 57
+On Two Types of Happiness
+
+happiness you get from succumbing to a temptation is inferior.
+When all of your decision are aligned toward a better future(even if you're suffering at times because things don't go as planned or it's hard to maintain self discipline), you wake up excited and full of energy.
+
+#When a person can't find a deep sense of meaning they distract themselves with Pleasure.
