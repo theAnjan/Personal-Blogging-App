5e55bff0efe0a63beea2d0b17e4e8c9abb7d683a
ANJANPAUDEL <anjan.paudel@azrachit.com>
2022-07-12 21:30:35 +0545

    day 56

---
+++ lib/upto60/day-56.dart
@@ -0,0 +1,7 @@
+On thinking You Can
+
+We often limit ourselves because we don't think we can achieve something. It's only subjective opinion, not a fact of life.
+Thinking you're unlikely to win means that you'll put in less effort than you could. Why would you give your whole potential if you are uncertain of success.
+When you think it's beyond your abilities, you won't do your best.
+You'll most likely give up when you encounter the first obstracle while a person thinking they can do it will fight as hard as they can to keep climbing. In the end, the person that doesn't believe in their abilities will give up along the way while the person that is certain of their abilities will reach the top.
+Periodically try things that you think are outside of your ability with a positive attitude and the belief that you can succeed, chances are you'll get a positive surprise and achieve something you thought was beyond your reach.
